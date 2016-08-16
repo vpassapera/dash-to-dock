bde35db8656aa9b063522a888337ede8a8714339
franglais125 <franglais125@gmail.com>
2016-08-16 14:54:46 -0400

    We probably want to keep a single variable for "forcedOverview". If the Applications icon is clicked on a secondary monitor, and then one navigates to the primary one, clicking ont he Applications icon again should close the overview.
    The button itself is not terribly useful on a secondary monitor.

--- docking.js
+++ docking.js
@@ -39,6 +39,11 @@ const State = {
     HIDING:  3
 };
 
+/* status variable: true when the overview is shown through the dash
+ * applications button.
+ */
+let forcedOverview = false;
+
 /**
  * A simple St.Widget with one child whose allocation takes into account the
  * slide out of its child via the _slidex parameter ([0:1]).
@@ -215,11 +220,6 @@ const DockedDash = new Lang.Class({
 
         // initialize dock state
         this._dockState = State.HIDDEN;
-
-        /* status variable: true when the overview is shown through the dash
-         * applications button.
-         */
-        this.forcedOverview = false;
 
         // Put dock on the primary monitor
         this._monitor = Main.layoutManager.primaryMonitor;
@@ -1257,7 +1257,7 @@ const DockedDash = new Lang.Class({
                 // force spring animation triggering.By default the animation only
                 // runs if we are already inside the overview.
                 if (!Main.overview._shown) {
-                    this.forcedOverview = true;
+                    forcedOverview = true;
                     if (animate) {
                         let view = Main.overview.viewSelector.appDisplay._views[visibleView].view;
                         let grid = view._grid;
@@ -1292,7 +1292,7 @@ const DockedDash = new Lang.Class({
                 Main.overview.show();
             }
             else {
-                if (this.forcedOverview) {
+                if (forcedOverview) {
                     // force exiting overview if needed
 
                     if (animate) {
@@ -1305,17 +1305,17 @@ const DockedDash = new Lang.Class({
                             Main.overview.viewSelector._appsPage.hide();
                             Main.overview.hide();
                             selector._showAppsButton.checked = false;
-                            this.forcedOverview = false;
+                            forcedOverview = false;
                         }));
                     }
                     else {
                         Main.overview.hide();
-                        this.forcedOverview = false;
+                        forcedOverview = false;
                     }
                 }
                 else {
                     selector._showAppsButton.checked = false;
-                    this.forcedOverview = false;
+                    forcedOverview = false;
                 }
             }
         }
@@ -1323,7 +1323,7 @@ const DockedDash = new Lang.Class({
         // whenever the button is unactivated even if not by the user still reset the
         // forcedOverview flag
         if (this.dash.showAppsButton.checked == false)
-            this.forcedOverview = false;
+            forcedOverview = false;
     },
 
     /**
