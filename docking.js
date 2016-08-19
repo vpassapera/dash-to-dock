e38ccaa8e05a99182fb5447e0a11dca244a72b88
franglais125 <franglais125@gmail.com>
2016-08-19 15:54:31 -0400

    We can move the signal 'changed::dock-position' to the dockManager. extension.js is much cleaner now.

--- docking.js
+++ docking.js
@@ -1596,6 +1596,10 @@ const DockManager = new Lang.Class({
             this._settings,
             'changed::preferred-monitor',
             Lang.bind(this, this._toggle)
+        ], [
+            this._settings,
+            'changed::dock-position',
+            Lang.bind(this, this._toggle)
         ]);
     },
 
